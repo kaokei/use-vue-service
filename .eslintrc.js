b29a3308472e3c2379a29db605c52c1e010f3182
kaokei <kaokei.rst@gmail.com>
2020-10-22 00:09:51 +0800

    mod: 添加eslint和prettier

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    //启用一组全局变量
+    browser: true,
+    es6: true,
+    node: true,
+    worker: true,
+    jest: true,
+  },
+  globals: {
+    //自定义的全局变量
+    window: true,
+    require: true,
+    process: true,
+    localStorage: true,
+    chrome: true,
+  },
+  settings: {
+    //自动发现React的版本，从而进行规范react代码
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    // 'prettier/babel',
+    // 'prettier/vue',
+    // 'prettier/standard',
+    // 'prettier/unicorn',
+    // 'prettier/flowtype',
+  ],
+  rules: {
+    'no-debugger': 2,
+    'no-console': 0,
+    'ban-ts-ignore': 0,
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+  },
+  overrides: [
+    //定制一组文件的规则
+    {
+      files: ['src/**/*.d.ts'],
+      rules: {
+        'no-unused-vars': 0,
+        '@typescript-eslint/no-unused-vars': 0,
+        '@typescript-eslint/triple-slash-reference': 0,
+      },
+    },
+  ],
+};
