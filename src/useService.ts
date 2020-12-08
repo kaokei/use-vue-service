8af12a471c75b29ff69d457468ca26932954c4e6
kaokei <kaokei.rst@gmail.com>
2020-12-08 23:14:12 +0800

    mod: useService支持数组参数

--- src/useService.ts
+++ src/useService.ts
@@ -2,12 +2,19 @@ import { inject, reactive } from 'vue';
 import { ServiceContext, DefaultContext } from './ServiceContext';
 import { IContextProps, getServiceInContext } from './getServiceInContext';
 
+type TupleCtr<T> = {
+  [P in keyof T]: T[P] extends new (...args: any) => infer R ? R : T[P];
+};
+
+type Ret<T> = T extends new (...args: any) => infer S ? S : TupleCtr<T>;
+
 export function useService<T = string>(Service: string, options?: any): T;
-export function useService<T, K = void>(
-  Service: new (...args: any[]) => K,
-  options?: any
-): K extends T ? K : T;
+export function useService<T = number>(Service: number, options?: any): T;
+export function useService<T>(Service: T, options?: any): Ret<T>;
 export function useService(Service: any, options?: any) {
+  if (Array.isArray(Service)) {
+    return Service.map(s => useService(s));
+  }
   const ctx = inject(ServiceContext, DefaultContext as IContextProps);
   const service = getServiceInContext(Service, ctx, options);
   return reactive(service);
