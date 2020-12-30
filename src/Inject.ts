c4aa22f0760ff736b4c44fa96789c5e53b95f9fa
kaokei <kaokei.rst@gmail.com>
2020-12-30 11:21:01 +0800

    mod: 支持类组件

--- src/Inject.ts
+++ src/Inject.ts
@@ -16,39 +16,127 @@
  *   实例访问器：原型, 方法名, 属性描述符
  */
 
+import { Options } from 'vue-class-component';
+import { declareProviders } from './declareProviders';
+import { useService } from './useService';
+
 export const PARAM_TYPES = 'service:paramtypes';
 export const DESIGN_PARAM_TYPES = 'design:paramtypes';
+export const DESIGN_TYPE = 'design:type';
 
+export interface InjectType {
+  name: string | number;
+  type: any;
+}
+
+type InjectTypes = Array<InjectType>;
+
+/**
+ * 可以使用在类构造函数的参数中和类的实例属性中
+ *
+ * @export
+ * @param {*} serviceIdentifier
+ * @return {*} void
+ */
 export function Inject(serviceIdentifier: any) {
   return function (target: any, targetKey: string, index?: number): void {
-    if (serviceIdentifier === undefined) {
-      throw new Error('未提供serviceIdentifier参数');
-    }
+    const Ctor = typeof target === 'function' ? target : target.constructor;
+
+    const metadata = Reflect.getMetadata(PARAM_TYPES, Ctor, targetKey) ||
+      Reflect.getMetadata(DESIGN_PARAM_TYPES, Ctor, targetKey) || {
+        index: [],
+        name: [],
+      };
 
     if (typeof index === 'number') {
-      const metadata =
-        Reflect.getMetadata(PARAM_TYPES, target, targetKey) ||
-        Reflect.getMetadata(DESIGN_PARAM_TYPES, target, targetKey);
-      metadata[index] = serviceIdentifier;
+      metadata.index[index] = { name: index, type: serviceIdentifier };
       Reflect.defineMetadata(PARAM_TYPES, metadata, target, targetKey);
+    } else {
+      metadata.name.push({
+        name: targetKey,
+        type: serviceIdentifier || Reflect.getMetadata(DESIGN_TYPE, target, targetKey),
+      });
     }
   };
 }
 
+/**
+ * 表明服务可注入
+ * 主要工作就是收集构造函数的参数类型信息
+ *
+ * @export
+ * @return {*}
+ */
 export function Injectable() {
-  return function (target: any) {
+  return function (target: any): void {
     if (Reflect.hasOwnMetadata(PARAM_TYPES, target)) {
       throw new Error('重复装饰器错误');
     }
 
     const metadata = Reflect.getMetadata(PARAM_TYPES, target);
     if (metadata) {
-      return target;
+      return;
     }
 
     const types = Reflect.getMetadata(DESIGN_PARAM_TYPES, target) || [];
-    Reflect.defineMetadata(PARAM_TYPES, types, target);
+    const newTypes = types.map((type: any, index: number) => ({
+      name: index,
+      type: type,
+    }));
+    Reflect.defineMetadata(PARAM_TYPES, newTypes, target);
+  };
+}
 
-    return target;
+/**
+ * 禁止在组件的构造函数中声明依赖注入
+ * 因为类组件需要继承Vue，Vue已经定义了自己的构造函数，导致子类必须在构造函数中手动调用super(props, ctx);
+ * 第二点就是组件在实例化的时候，已经写死了`new Ctor(props, ctx);`
+ * 没有办法实现依赖注入，假设vue-class-component库支持传递多余的参数`new Ctor(props, ctx, ...rest);`
+ * 我认为数据不需要proxy，只需要reactive即可
+ *
+ * 这里是代理了Options的功能
+ *
+ * 并且仿造了createDecorator的逻辑来代理setup函数
+ *
+ * @export
+ * @return {*}
+ */
+export function Component(options: any = {}) {
+  return function (target: any) {
+    const { providers } = options;
+    delete options.providers;
+
+    Options(options)(target); // 使用@Component代替@Options
+
+    if (!target.__d) {
+      target.__d = [];
+    }
+    target.__d.push((options: any) => {
+      const originSetup = options.setup;
+
+      options.setup = (props: any, ctx: any) => {
+        if (providers) {
+          declareProviders(providers);
+        }
+        let result = originSetup ? originSetup(props, ctx) : {};
+        const metadata = Reflect.getMetadata(PARAM_TYPES, target);
+        const services = useService(metadata.map((item: any) => item.type));
+
+        if (result instanceof Promise) {
+          result = result.then(res => {
+            metadata.forEach(
+              (item: any, index: number) => (res[item.name] = services[index])
+            );
+            return res;
+          });
+        } else {
+          metadata.forEach(
+            (item: any, index: number) => (result[item.name] = services[index])
+          );
+        }
+
+        return result;
+      };
+    });
   };
 }
--- src/declareProviders.ts
+++ src/declareProviders.ts
@@ -1,6 +1,19 @@
 import { provide, inject } from 'vue';
 import { ServiceContext, DefaultContext } from './ServiceContext';
 
+/**
+ * 类组件可以通过装饰器声明providers，内部实际上也是调用的declareProviders方法
+ * 但是options组件或者defineComponent组件则必须在setup函数中手动调用declareProviders方法
+ * 我有考虑过增加一个option，比如providers，就像vue-rx增加了subscriptions属性一样。
+ * 最终没有实现，是因为一方面实现成本有点高（我不会。。。）
+ * 还有一种思路就是包装defineComponent，代理setup函数，把providers属性转为declareProviders函数调用。
+ * 这种做法的成本是最低的，但是有两点限制，第一用户可以不使用defineComponent来定义组件，第二用户得转变使用习惯，从我的包里面导入defineComponent
+ * 还考虑到官网的示例代码中也是直接在setup函数中调用provide函数，而且vue2.x版本中就是采用的provide/inject属性来配置。vue3中已经转为函数了
+ * 我觉得我也没有必要一定为了追求声明式代码，强制实现声明式功能
+ *
+ * @export
+ * @param {any[]} providers
+ */
 export function declareProviders(providers: any[]) {
   const parentCtx = inject(ServiceContext, DefaultContext);
   const newProviders = providers.map(p => {
