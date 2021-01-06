390a42b6463d5bdf935a6ee4582609c86fa6b298
kaokei <kaokei.rst@gmail.com>
2021-01-06 21:47:28 +0800

    mod: 重构代码结构

--- src/ServiceContext.ts
+++ src/ServiceContext.ts
@@ -1,6 +1,10 @@
 export const ServiceContext = 'USE_VUE_SERVICE_CONTEXT';
 export const DefaultContext = {};
 
-export const PARAM_TYPES = 'service:paramtypes';
 export const DESIGN_PARAM_TYPES = 'design:paramtypes';
 export const DESIGN_TYPE = 'design:type';
+
+export const SERVICE_PARAM_TYPES = 'service:paramtypes';
+export const SERVICE_INJECTED_PARAMS = 'service:injected:params';
+export const SERVICE_INJECTED_PROPS = 'service:injected:props';
+export const SERVICE_INJECTED_KEY = 'service:injected:key';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { Inject } from './Inject'; // 参数或者属性与服务进行关联
+export { Inject, Skip, Namespace } from './Inject'; // 参数或者属性与服务进行关联
 export { Injectable } from './Injectable'; // 类可以注入其他类
 export { Component } from './Component'; // 组件可以注入其他类
 export { declareProviders } from './declareProviders'; // 声明服务提供者
-export { useService } from './useService'; // 获取服务实例
+export { useService } from './getServiceInContext'; // 获取服务实例
