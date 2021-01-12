f501b1eacdb5b43f6ce1b8f5a05cc69fe1e03ffc
kaokei <kaokei.rst@gmail.com>
2021-01-13 02:54:42 +0800

    mod: 重新调整vue文件的类型定义

--- test/data/Person.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '../../src/Injectable';
-import Logger from './Logger.service';
-
-@Injectable()
-export default class Person {
-  constructor(public name: string, public age: number, public logger: Logger) {}
-}
