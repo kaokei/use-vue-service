01d687286ba67e7d80c1a91e3e9ad11932fec9e5
kaokei <kaokei.rst@gmail.com>
2021-05-25 16:45:18 +0800

    尝试使用di库

--- src_backup/index.ts
+++ src_backup/index.ts
@@ -1,4 +1,5 @@
+export { Inject, Skip, Namespace } from './Inject'; // 参数或者属性与服务进行关联
+export { Injectable } from './Injectable'; // 类可以注入其他类
 export { Component } from './Component'; // 组件可以注入其他类
 export { declareProviders } from './declareProviders'; // 声明服务提供者
 export { useService } from './useService'; // 获取服务实例
-export * from '@kaokei/di';
