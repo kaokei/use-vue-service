6064c4e1b7fcd102f287bea03a1984c1621d2884
kaokei <kaokei.rst@gmail.com>
2021-08-22 16:17:09 +0800

    feat(update): 升级@kaokei/di

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 // 导出依赖注入需要的装饰器
-export {
-  Inject,
-  Self,
-  Skip,
-  Optional,
-  Injectable,
-  forwardRef,
-} from '@kaokei/di';
+export * from '@kaokei/di';
 
 // setup中声明服务提供者
 export { declareProviders } from './declareProviders';
@@ -17,7 +10,7 @@ export { useService } from './useService';
 // 在createApp返回的app实例上挂载Injector
 export { createVuePlugin } from './createVuePlugin';
 
-// 以下到处不是面向普通用户使用的
-// 而是面向第三方库的开发者，比如我开发的支持类组件的库
+// 以下导出不是面向普通用户使用的
+// 而是面向第三方库的开发者，比如自己开发的支持类组件的库
 export { INJECTOR_KEY, DEFAULT_INJECTOR } from './constants';
 export { injectFromSelf } from './fakeInject';
