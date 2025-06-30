9d2268e52a508daf06a14841ac8001f3a000ba7b
kaokei <kaokei.rst@gmail.com>
2025-06-30 11:35:06 +0800

    增加vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: '@kaokei/use-vue-service',
+  description:
+    'Lightweight Vue 3 state management with dependency injection, inspired by Angular services.',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'API', link: '/api/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Note', link: '/note/' },
+    ],
+
+    sidebar: {
+      '/note/': [
+        {
+          text: 'Note',
+          link: '/note/',
+          items: [
+            {
+              text: '父组件与子组件的理解',
+              link: '/note/01.父组件与子组件的理解',
+            },
+            {
+              text: '子组件获取父组件服务',
+              link: '/note/02.子组件获取父组件服务',
+            },
+            {
+              text: '父组件获取子组件服务',
+              link: '/note/03.父组件获取子组件服务',
+            },
+            { text: '不同场景', link: '/note/04.不同场景' },
+            { text: '响应式方案', link: '/note/05.响应式方案' },
+            { text: '装饰器', link: '/note/06.装饰器' },
+            { text: 'SSR-deepseek', link: '/note/07.SSR-deepseek' },
+            { text: 'SSR-angular', link: '/note/08.SSR-angular' },
+            { text: 'SSR-pinia', link: '/note/09.SSR-pinia' },
+            { text: 'SSR', link: '/note/10.SSR' },
+            { text: '基本命令', link: '/note/99.基本命令' },
+          ],
+        },
+      ],
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/kaokei/use-vue-service' },
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: '@kaokei/use-vue-service'
+  text: '​Vue 3 状态管理 + 依赖注入'
+  tagline: vuex/pinia的替代品，灵感来自于 Angular Service。
+  actions:
+    - theme: brand
+      text: Guide
+      link: /guide/
+    - theme: alt
+      text: API
+      link: /api/
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
