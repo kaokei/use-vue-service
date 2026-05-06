6c82caf1dd6ff1a3169a3636a087e8c706b2b83c
kaokei <kaokei.rst@gmail.com>
2026-05-06 15:35:28 +0800

    优化示例demo中服务的实现

--- examples/20-devtools-demo/src/UserService.ts
+++ examples/20-devtools-demo/src/UserService.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@kaokei/use-vue-service'
-import { ref } from 'vue'
 
 @Injectable()
 export class UserService {
-  name = ref('张三')
-  role = ref('admin')
+  // 实例被 reactive() 包裹，普通属性即为响应式，无需 ref()
+  name = '张三'
+  role = 'admin'
 
   setName(name: string) {
-    this.name.value = name
+    this.name = name
   }
 }
