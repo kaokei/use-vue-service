fbbd92b6941691e582007da7336853800494a6d4
kaokei <kaokei.rst@gmail.com>
2026-05-07 17:19:51 +0800

    发布4.0.4版本

--- examples/13-nuxt-decorators/nuxt.config.ts
+++ examples/13-nuxt-decorators/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   experimental: {
     // 启用 TC39 Stage 3 装饰器支持
     decorators: true,
+    // 修复 Nuxt 4.4.4 中 ssr:false 时 "Vite Node IPC socket path not configured" 的问题
+    // 参考: https://github.com/nuxt/nuxt/issues/34957
+    viteEnvironmentApi: true,
   },
 
   // 关闭 SSR，方便本地演示
